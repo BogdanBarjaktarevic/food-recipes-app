42d08536dd60b473f5a6c83fb51dfc8bbe6e62ac
barjaktarevicbogdan@gmail.com <barjaktarevicbogdan@gmail.com>
2019-06-27 16:16:27 +0200

    testing

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { shallow } from "enzyme";
+import App from '../App';
+import Homepage from '../../pages/Homepage';
+
+it("renders Homepage", () => {
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(Homepage).length).toEqual(1);
+});
--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
 
-import reducers from "./store/reducers/index";
+import Root from './Root';
 import App from './components/App';
 
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, /* preloadedState, */ composeEnhancers(
-    applyMiddleware(thunk)
-  ));
-
 ReactDOM.render(
-    <Provider store={store}>
+    <Root>
         <App />
-    </Provider>,
+    </Root>,
     document.querySelector('#root')
 )
